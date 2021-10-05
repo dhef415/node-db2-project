ed9cf64ac2b2bbf8dce5c205368bf6a428ea5d07
dhef415 <dhef415@gmail.com>
2021-10-05 11:22:08 -0700

    schema built

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
-};
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', tbl => {
+    tbl.increments('car_id')
+    tbl.text('vin', 128).unique().notNullable()
+    tbl.text('make', 128).notNullable()
+    tbl.text('model', 128).notNullable()
+    tbl.integer('mileage').notNullable()
+    tbl.text('title', 128)
+    tbl.text('transmission', 128)
+  })
+}
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
-};
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
+}
